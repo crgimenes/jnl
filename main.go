5f390e0bb2a81b45618ba6d2a97048d14a3b6394
crgimenes <crg@crg.eti.br>
2025-05-08 05:05:44 -0300

    read config lua file

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/crgimenes/jnl/lua"
 	"golang.org/x/term"
 )
 
@@ -12,7 +14,49 @@ var (
 	isTTY = term.IsTerminal(int(os.Stdout.Fd()))
 )
 
+func getInitLuaPath() string {
+	configHome := os.Getenv("XDG_CONFIG_HOME")
+	if configHome == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal("Failed to get home directory:", err)
+		}
+		configHome = filepath.Join(home, ".config")
+	}
+	return filepath.Join(configHome, "jnl", "init.lua")
+}
+
+func runLuaFile(name string) {
+	// Create a new Lua state.
+	L := lua.New()
+	defer L.Close()
+
+	// Read the Lua file.
+	b, err := os.ReadFile(filepath.Clean(name))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = L.DoString(string(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		panic(err)
+	}
+	return true
+}
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+
 	// check first parameter
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a command to run.")
@@ -20,6 +64,15 @@ func main() {
 	}
 
 	cmd := os.Args[1]
+
+	initFile := getInitLuaPath()
+	if fileExists(initFile) {
+		runLuaFile(initFile)
+	} else {
+		// load local config
+		initFile = "./jnl_init.lua"
+		runLuaFile(initFile)
+	}
 
 	switch cmd {
 	case "add":
@@ -49,10 +102,28 @@ func main() {
 		log.Println("not implemented")
 		return
 	case "publish":
-		// view the note in cat
+		log.Println("not implemented")
 		return
 	case "review":
-		// view the note in cat
+		log.Println("not implemented")
+		return
+	case "help":
+		log.Println("not implemented")
+		return
+	case "version":
+		log.Println("not implemented")
+		return
+	case "config":
+		log.Println("not implemented")
+		return
+	case "init":
+		log.Println("not implemented")
+		return
+	case "serve": // start a web server
+		log.Println("not implemented")
+		return
+	case "sync": // sync with remote server
+		log.Println("not implemented")
 		return
 	default:
 		fmt.Println("Unknown command:", cmd)
